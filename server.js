a764981d107c5efb716115cc4badf4ffb4c3128b
imsashwat <star1sashwat@gmail.com>
2019-07-27 17:12:32 +0530

    final commit

--- server.js
+++ server.js
@@ -62,4 +62,4 @@ app.use('/public', require('./routes/public'))
 app.use('/private', require('./routes/private'))
 app.use('/', require('./routes/root'))
 
-server.listen(9876, () => console.log("Server running on http://localhost:9876"))
+server.listen(process.env.PORT || 8000)
