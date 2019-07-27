e7b8f01bc645affcd0a645bd56ccdee6ecd0ff4f
imsashwat <star1sashwat@gmail.com>
2019-07-27 21:43:17 +0530

    sq

--- db.js
+++ db.js
@@ -1,14 +1,9 @@
 const Sequelize = require('sequelize');
 
-const db = new Sequelize(
-    'userdb',
-    'userdb',
-    'userdb',
-    {
-        dialect: 'mysql',
-        host: 'localhost'
-    }
-)
+const db = new Sequelize(process.env.DATABASE_URL, {    
+        dialect: 'postgres',        
+    });
+
 
 const Users = db.define('users', {
     username: {
